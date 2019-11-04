8adb17b3993e63c8d982cbc919422ba562c8cfe0
yemi <zcyemi@gmail.com>
2019-11-04 11:37:04 +0800

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
 gulp.task("start",()=>{
+
+    gulp.src('./samples/**').pipe(gulp.dest('./dist'));
     browserSync.init({
         server: "./dist",
         files: "./dist/**/**.*"
